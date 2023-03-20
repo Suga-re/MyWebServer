23cd42b680901d970d8ea7ab746837e4d77efbad
azheng04 <azheng04@wm.edu>
2023-03-20 14:19:02 -0400

    Read get request line

--- src/MyWebServer.java
+++ src/MyWebServer.java
@@ -18,17 +18,34 @@ public class MyWebServer {
             System.out.println("Client connected to server");
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 
-            while((clientSentence = inFromClient.readLine())!=null){
-                System.out.println(clientSentence);
-                if(clientSentence.isEmpty()){
-                    break;
+
+            clientSentence = inFromClient.readLine();
+            System.out.println(clientSentence);
+
+
+            /*
+            Parse GET request
+             */
+            String[] request = clientSentence.split(" ", 3);
+            if (request[0].equals("GET")){
+                System.out.println(request.length);
+                String extension=request[1];
+
+                if(extension.equals("/")){
+                    System.out.println("main page");
                 }
-            };
+                else{
+                    System.out.println(request[1]);
+                    //get the files and display it for the page
+                }
+            }
+
+
+
+
 
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-
-            outToClient.write("Hello World".getBytes());
-//            connectionSocket.close();
+           connectionSocket.close();
         }
 
     }
