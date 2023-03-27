50c80cde3214c5bd136a156d29e0e5892104c6a9
azheng04 <azheng04@wm.edu>
2023-03-26 23:20:35 -0400

    HTTP header formatted and bytes written to dos

--- src/MyWebServer.java
+++ src/MyWebServer.java
@@ -1,18 +1,32 @@
 package src;
 import java.io.*;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MyWebServer {
+/*
+Variables
+ */
+    static String path;
+    static File f;
+    static String statusCode;
+
+    static Date date;
+    static String serverName="Johnny's Server";
+    static long lastModified;
+    static long contentLength=0;
+
     public static void main(String[] args) throws Exception {
+
         int port = Integer.parseInt(args[0]);
         ServerSocket welcomeSocket = new ServerSocket(port);
-
-        String path = args[1];
         System.out.println("server is running on port "+ port);
 
         String clientSentence;
 
         while(true){
+
             Socket connectionSocket = welcomeSocket.accept();
             System.out.println("Client connected to server");
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
@@ -22,30 +36,9 @@ public class MyWebServer {
             System.out.println(clientSentence);
 
 
-            /*
-            Parse GET request
-             */
-            String[] request = clientSentence.split(" ", 3);
-            if (request[0].equals("GET") && request[2].equals("HTTP/1.1")){ //For get requests
-                System.out.println(request.length);
-                String extension=request[1];
 
-                if(extension.equals("/") || extension.equals("/favicon.ico")){
-                    path +="/index.html";
-                    System.out.println(path);
-                }
-                else{
-                    path += extension;
-                    System.out.println(path);
-                    //get the files and display it for the page
-                }
-            }
-            else if (request[0].equals("HEAD")){ //for head requests
-
-            }
-            else{
-                //Insert Error code Command 501 Not Implemented
-            }
+           path=fullFilePath(args[1],clientSentence);
+           f=new File(path);
 
             FileInputStream fis=null;
             boolean fileFound=true;
@@ -58,41 +51,90 @@ public class MyWebServer {
             }
 
 
-
-
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-//            outToClient.writeBytes(sampleResponse);
+
 
             if(fileFound)
             {
-                String statusLine = "HTTP/1.0 200 OK\r\n";
-                outToClient.writeBytes(statusLine);
-                sendBytesToClient(fis, outToClient);
+                statusCode = "HTTP/1.1 200 OK";
+
+                f= new File(path);
+                String header= formatHeader(statusCode, f);
+                System.out.print(header);
+                if (clientSentence.contains("HEAD")){
+                    outToClient.writeBytes(header);
+                }
+                else if (clientSentence.contains("GET")){
+                    outToClient.writeBytes(header);
+                    sendBytesToClient(fis, outToClient);
+                }
             }
             else{
                 System.out.print("file not found");
             }
-
-            path = args[1]; //reset path
-
+            System.out.print("\n");
            connectionSocket.close();
         }
 
     }
-    //send the file byte by byte to client
-    private static void sendBytesToClient( FileInputStream fis, OutputStream os) throws
-            Exception
-    {
-        byte[] buffer = new byte[1024];
-        int bytes = 0;
 
-        while((bytes = fis.read(buffer)) != -1)
-        {
+
+    private static void sendBytesToClient( FileInputStream fis, DataOutputStream os) throws
+            Exception      //send the file byte by byte to client
+    {
+
+        int bytes;
+//        BufferedReader br = new BufferedReader(new InputStreamReader(fis)); // second parameter is Character set
+        byte[] buffer = new byte[(int)contentLength];
+        System.out.println("this is content length: "+contentLength);
+
+        while((bytes = fis.read(buffer)) > 0) { //if there is a byte to read then write to output strea,
             os.write(buffer, 0, bytes);
+            os.flush();
         }
     }
+    /*
+    returns path is request is valid and returns 501 error code if not a proper request
 
-    private static String fileType(String fileString){
-        return "yes";
+     */
+    private static String fullFilePath(String rootPath,String clientSentence){
+        String path = rootPath;
+        /*
+            Parse GET request
+             */
+        String[] request = clientSentence.split(" ", 3);
+        if (request[2].equals("HTTP/1.1")){
+            String extension=request[1];
+
+            if(extension.equals("/") || extension.equals("/favicon.ico") ){
+                path +="/index.html";
+                System.out.println(path);
+            }
+            else{
+                path += extension;
+                System.out.println(path);
+            }
+        }
+
+        return(path);
     }
+    private static String formatHeader(String statusCode, File f){ //header formatter
+        SimpleDateFormat HTTPDateFormat = new SimpleDateFormat("EEE MMM d hh:mm:ss zzz yyyy");
+
+        date = new Date(); //set date to request date and time
+        String formatDate= HTTPDateFormat.format(date);
+        contentLength=f.length();
+        lastModified= f.lastModified();
+        String formatLastModified = HTTPDateFormat.format(lastModified);
+
+        String response= statusCode + '\n' +
+                "Date: " + formatDate + "\n" +
+                "Server: " + serverName + "\n" +
+                "Last-Modified:" + formatLastModified + "\n" +
+                "Content-Length:" + contentLength +"\n\n";
+
+        return response;
+
+    }
+
 }
